6c9e3acbe33c089ccd92dd3781aaa384bfdc27e1
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-02-18 16:49:50 +0300

    foto ekleme özelliği eklendi

--- kod.js
+++ kod.js
@@ -1,12 +1,16 @@
 const newPlayer = document.getElementById("titleYeni");
 
 const textBox = document.getElementById("floatingTextarea");
-
-
+const textPhotoBox = document.getElementById("fotoArea");
+const img3 = document.getElementById("img3");
 
 function buttonClicked() {
     newPlayer.innerHTML = textBox.value;
 
     textBox.value = "";
     textBox.placeholder = "yeni bir isim giriniz..."
+}
+
+function PhotoButtonClicked() {
+    img3.src = textPhotoBox.value;
 }
